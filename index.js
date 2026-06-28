6d1c381766e095d3261e4d096b612a2aea54274b
Sithila Akalanka <sithilaakalanka36@gmail.com>
2026-06-28 12:19:22 +0530

    6/28 Node App

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ status: 'ok', session: 'NB6007CEM S2' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
